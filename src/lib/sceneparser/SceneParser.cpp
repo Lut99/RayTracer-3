46e77376ba50fd30cc897cb6fc1b53231e728f51
Lut99 <mullertim99@gmail.com>
2021-05-06 18:34:38 +0200

    Started work on the scenefile parser

---
+++ src/lib/sceneparser/SceneParser.cpp
@@ -0,0 +1,15 @@
+/* SCENE PARSER.cpp
+ *   by Lut99
+ *
+ * Created:
+ *   06/05/2021, 18:32:23
+ * Last edited:
+ *   06/05/2021, 18:32:26
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Serves as the entry point for the parser for our .scene files.
+**/
+
+
---
+++ src/lib/sceneparser/SceneParser.hpp
@@ -0,0 +1,26 @@
+/* SCENE PARSER.hpp
+ *   by Lut99
+ *
+ * Created:
+ *   06/05/2021, 18:32:11
+ * Last edited:
+ *   06/05/2021, 18:34:25
+ * Auto updated?
+ *   Yes
+ *
+ * Description:
+ *   Serves as the entry point for the parser for our .scene files.
+**/
+
+#ifndef SCENEPARSER_SCENE_PARSER_HPP
+#define SCENEPARSER_SCENE_PARSER_HPP
+
+namespace RayTracer::SceneParser {
+    /* The SceneParser class, which is used to obtain a list of RenderEntities from a .scene file. It is designed such that render entities may define how they're parsed at render start. */
+    class SceneParser {
+          
+    };
+    
+}
+
+#endif
